c2365ef4442754dc2a06354a7a924250c217f506
BykovDenis2000 <denbykov@mail.ru>
2020-12-29 20:30:32 +0300

    fix commit

--- src/main/java/com/titles/databaseservice/cold/ColdRepository.java
+++ src/main/java/com/titles/databaseservice/cold/ColdRepository.java
@@ -1,10 +1,11 @@
-package com.titles.databaseservice;
+package com.titles.databaseservice.cold;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
+import com.titles.databaseservice.Model.ColdPaper;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface ColdRepository extends CrudRepository<ColdCustomer, Long> {
+public interface ColdRepository extends CrudRepository<ColdPaper, Long> {
 }
